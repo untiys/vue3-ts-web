5891e210bce2095ee4c6ed441c2b4dd58b243068
然也 <88367380+untiys@users.noreply.github.com>
2023-05-11 13:50:27 +0800

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -45,21 +45,26 @@ const router = createRouter({
 })
 
 // 路由守卫
-// router.beforeEach((to, from, next) => {
-
-//     const token = localStorage.getItem('token')
-//     if (!token) {
-//         if (to.name === 'login') {
-//             next()
-//         }
-//         else {
-//             next('/login')
-//         }
-//     } else {
-//         // 断言类型
-//         document.title = to.meta.title as string
-//         next()
-//     }
-// })
+router.beforeEach((to, from, next) => {
+  window?.axiosPromiseArr?.forEach((ele: any, index: number) => {
+    ele.cancel() // 路由跳转之前，终止上一个页面正在请求的内容
+    // 清空请求的参数
+    delete window.axiosPromiseArr[index]
+  })
+  document.title = to.meta.title as string
+  next()
+  // const token = localStorage.getItem("token")
+  // if (!token) {
+  //   if (to.name === "login") {
+  //     next()
+  //   } else {
+  //     next("/login")
+  //   }
+  // } else {
+  //   // 断言类型
+  //   document.title = to.meta.title as string
+  //   next()
+  // }
+})
 
 export default router
